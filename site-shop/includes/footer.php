c03489d43cad8a08fd8b99034874d435c9581158
FuIka <71216702+FuIka@users.noreply.github.com>
2025-06-05 01:23:39 +0700

    Delete site-shop directory

--- site-shop/includes/footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-<footer style="background-color: #f8f8f8; padding: 20px 0; text-align: center; font-family: Arial, sans-serif; font-size: 14px; color: #555;">
-  <p>&copy; 2025 Магазин. Все права защищены.</p>
-  <p>Разработчик Fulka.</p>
-</footer>
-</body>
-
-</html>
